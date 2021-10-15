a6363e7f13313fd7e7fcdee5a52a6f30eb02f59e
Bradley Wayne Arnold <82877822+B1r2a3d4l5y@users.noreply.github.com>
2021-10-15 10:37:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from database import Database
-from models.post import Post
+from models.blog import Blog
 
 Database.initialize()
 
-post = Post(blog_id="123",
-            title="Another great post",
-            content="This is some sample content",
-            author="Brad")
+blog = Blog(author="Brad",
+            title="Sample title",
+            description="Sample description")
 
-post_save_to_mongo()
+blog.new_post()
+
+blog.save_to_mongo()
+
+from_database = Blog.from_mongo(blog.id)
+
+print(blog.get_posts())
