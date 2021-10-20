24f05a076b9945f6de1a7ba915d7a440080dbbf7
Bradley Wayne Arnold <82877822+B1r2a3d4l5y@users.noreply.github.com>
2021-10-20 15:12:31 +0200

    Update blog.py

--- models/blog.py
+++ models/blog.py
@@ -45,8 +45,8 @@ class Blog(object):
 
     @classmethod
     def from_mongo(cls, id):
-        blog_data = Database.find_one(collection='blogs',
-                                      query={'id': id})
+        blog_data = Database.find(collection='blogs',
+                                  query={'id': id})
         return cls(author=blog_data['author'],
                    title=blog_data['title'],
                    description=blog_data['description'],
